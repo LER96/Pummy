f71e68cf8a67b397a426c685493c8a560f37dbe2
TalTsadikov <taltsadikov@gmail.com>
2022-08-14 17:04:00 +0300

    created item scriptable object script

---
+++ Assets/Scripts/Tal's scene/item.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item", menuName ="Item/Create New Item")]
+public class item : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+}
