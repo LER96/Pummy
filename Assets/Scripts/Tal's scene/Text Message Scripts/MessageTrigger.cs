6b6843a013e0db4bdb5b04a20a8a40e9bac9c505
TalTsadikov <taltsadikov@gmail.com>
2022-10-22 17:00:11 +0300

    text message event system update

---
+++ Assets/Scripts/Tal's scene/Text Message Scripts/MessageTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageTrigger : MonoBehaviour
+{
+    [SerializeField] private GameEvent OnPlayerDetected;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            OnPlayerDetected.Raise();
+        }
+    }
+}
---
+++ Assets/Scripts/Tal's scene/Text Message Scripts/TextMessageScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextMessageScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI messageText;
+
+    [SerializeField] TextMeshProUGUI responeMessageText;
+
+    public void UpdateTextUI()
+    {
+        messageText.text = "hey how are you";
+        responeMessageText.text = "im good thank you for asking";
+    }
+}
