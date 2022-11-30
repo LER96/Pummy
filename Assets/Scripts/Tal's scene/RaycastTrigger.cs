b8245793c41431515a38c1121ab0d9432050c61c
TalTsadikov <taltsadikov@gmail.com>
2022-11-30 16:34:40 +0200

    pummy door animation event
    added pummy door animation event (still not working)

---
+++ Assets/Scripts/Tal's scene/RaycastTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTrigger : MonoBehaviour
+{
+    [SerializeField] private GameEvent OnRayHit;
+    [SerializeField] RaycastHit hit;
+    [SerializeField] LayerMask DadDoor;
+    [SerializeField] Animation anim;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, DadDoor))
+        {
+            OnRayHit.Raise();
+            Debug.Log("Triggered");
+        }
+    }
+
+    public void Something()
+    {
+        anim.Play();
+    }
+}
