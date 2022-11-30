a66728a7d08915f7f310ded220600d6ad262d9d3
layanmeta <layan.meta@gmail.com>
2022-11-30 14:32:45 +0200

    meh

---
+++ GameEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent : MonoBehaviour
+{
+    public static GameEvent current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+
+}
